887c76ddb9e74e89618188f983816b616657e0e6
OVOMARK <mcncstatefan@icloud.com>
2022-11-02 20:44:56 -0400

    started JS

---
+++ index.js
@@ -0,0 +1,29 @@
+const Inquirer = require('inquirer');
+const Jest = require('jest');
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the name of the manager of this team??',
+        name: 'Name'
+    },
+    {
+        type: 'input',
+        message: 'What is their employee ID?',
+        name: 'ID'
+    },
+    {
+        type: 'input',
+        message: 'What is their email?',
+        name: 'Email'
+    },
+    {
+        type: 'input',
+        message: 'What is their office number?',
+        name: 'officeNumber'
+    },
+    {
+        type: 'list',
+        message: 'Would you like to add an engineer or intern to your roster?',
+        choices: ['Engineer', 'Intern', 'No more members']
+    }
+]
