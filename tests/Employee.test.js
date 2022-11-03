023d73725178da63fdfc412137465c3402c00509
OVOMARK <mcncstatefan@icloud.com>
2022-11-03 13:20:32 -0400

    fix test 1 and employee constructor

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,39 +1,16 @@
-const Employee = require('..lib/Employee.js');
+const Employee = require('../lib/Employee')
 
 describe('Getting Employee Info', () => {
-    it('Grabs name', () => {
+    it('Grabs employee basics', () => {
         const name = 'Marcus';
-        const Employee = new Employee(name);
+        const email = 'marcus@gmail.com';
+        const id = 123;
+        const role = 'Manager';
+        const e = new Employee(name, email, id, role);
 
-        expect(Employee.name).toEqual(this.name)
-
-
-    })
-
-    it('Should be able to get email', () => {
-        it('Grabs email', () => {
-            const email = 'marcus@gmail.com';
-            const Employee = new Employee('Marcus', email);
-
-            expect(Employee.email).toEqual(email)
-        })
-    })
-
-    it('Should be able to get id', () => {
-        it('Grabs email', () => {
-            const id = 123
-            const Employee = new Employee('Marcus', 'marcus@gmail.com', id);
-
-            expect(Employee.id).toEqual(id)
-        })
-    })
-
-    it('Should be able to get role', () => {
-        it('Grabs email', () => {
-            const role = 'Manager';
-            const Employee = new Employee('Marcus', 'marcus@gmail.com', '123', role);
-
-            expect(Employee.role).toEqual(role)
-        })
+        expect(e.name).toEqual(name);
+        expect(e.email).toEqual(email);
+        expect(e.id).toEqual(id);
+        expect(e.role).toEqual(role);
     })
 })
