1f29a4c7781c277a0b8d290b2722f215ab36cf9f
OVOMARK <mcncstatefan@icloud.com>
2022-11-05 19:38:17 -0400

    began question prompting

--- index.js
+++ index.js
@@ -3,3 +3,35 @@ const Jest = require('jest');
 const fs = require('fs');
 const Employee = require('./lib/Employee')
 
+function init () {
+    Inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the name of this employee?',
+                name: 'Name'
+            },
+            {
+                type: 'input',
+                message: 'What is their employee ID?',
+                name: 'ID'
+            },
+            {
+                type: 'input',
+                message: 'What is their email?',
+                name: 'Email'
+            },
+            {
+                type: 'input',
+                message: 'What is their office number?',
+                name: 'officeNumber'
+            },
+            {
+                type: 'list',
+                message: 'Would you like to add an engineer or intern to your roster?',
+                choices: ['Engineer', 'Intern', 'No more members'],
+                name: 'role'
+            }])
+        }
+
+init();
