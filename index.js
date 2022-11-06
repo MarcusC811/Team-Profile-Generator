746e2bfab0a5025c5d717a72abfe250dc3b9a922
OVOMARK <mcncstatefan@icloud.com>
2022-11-05 21:31:09 -0400

    more index.js

--- index.js
+++ index.js
@@ -2,36 +2,85 @@ const Inquirer = require('inquirer');
 const Jest = require('jest');
 const fs = require('fs');
 const Employee = require('./lib/Employee')
+let questions = [{
+    type: 'input',
+    message: 'What is your name?',
+    name: 'Name'
+},
+{
+    type: 'input',
+    message: 'What is their employee ID?',
+    name: 'ID'
+},
+{
+    type: 'input',
+    message: 'What is their email?',
+    name: 'Email'
+}]
 
 function init () {
-    Inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: 'Who is the manager of this team?',
-                name: 'manName'
-            },
-            {
-                type: 'input',
-                message: 'What is their employee ID?',
-                name: 'manID'
-            },
-            {
-                type: 'input',
-                message: 'What is their email?',
-                name: 'manEmail'
-            },
-            {
-                type: 'input',
-                message: 'What is their office number?',
-                name: 'manofficeNumber'
-            },
-            {
-                type: 'list',
-                message: 'Would you like to add an engineer or intern to your roster?',
-                choices: ['Engineer', 'Intern', 'No more members'],
-                name: 'role'
-            }])
-        }
+    let team = []
 
+    let emQuestions =  Inquirer
+        .prompt([{
+            type: 'input',
+            message: 'What is your name?',
+            name: 'Name'
+        },
+        {
+            type: 'input',
+            message: 'What is their employee ID?',
+            name: 'ID'
+        },
+        {
+            type: 'input',
+            message: 'What is their email?',
+            name: 'Email'
+        },
+        {
+            type: 'list',
+            message: 'What is your role?',
+            choices: ['Manager', 'Engineer', 'Intern', 'No more members'],
+            name: 'role'
+        }])
+        
+        if(answer.role === 'Engineer') {
+            const engQ = Inquirer
+             .prompt([
+                {
+                    type: 'input',
+                    message: 'What is your GitHub username?',
+                    name: 'GitHub'
+                }
+            ])
+        } else if(answer.role === 'Intern') {
+            const intQ = Inquirer
+             .prompt([
+                {
+                    type: 'input',
+                    message: 'What school do you attend?',
+                    name: 'school'
+                }
+            ])
+        } else if(answer.role === 'Manager') {
+            const manQ = Inquirer
+             .prompt([
+                {
+                    type: 'input',
+                    message: 'What is your office number?',
+                    name: 'offNum'
+                }
+            ])
+        } else if(!answer.role) {emQuestions}
+
+}
 init();
+
+// {
+//     type: 'input',
+//     message: 'What is their office number?',
+//     name: 'manofficeNumber'
+// }
+
+
+
