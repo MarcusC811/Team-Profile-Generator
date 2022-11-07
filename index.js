678c845060efb71248b452421c8ffaef545d771f
OVOMARK <mcncstatefan@icloud.com>
2022-11-06 21:58:05 -0500

    more code clean up

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+// Dependencies
 const Inquirer = require('inquirer');
 const Jest = require('jest');
 const fs = require('fs');
+// Class Libraries
 const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+
 let questions = [{
     type: 'input',
     message: 'What is your name?',
@@ -25,28 +28,7 @@ function init () {
     let team = []
 
         Inquirer
-        .prompt([
-        {
-            type: 'input',
-            message: 'What is your name?',
-            name: 'Name'
-        },
-        {
-            type: 'input',
-            message: 'What is their employee ID?',
-            name: 'ID'
-        },
-        {
-            type: 'input',
-            message: 'What is their email?',
-            name: 'Email'
-        },
-        {
-            type: 'list',
-            message: 'What is your role?',
-            choices: ['Manager', 'Engineer', 'Intern', 'No more members'],
-            name: 'role'
-        }])
+        .prompt(questions)
         .then((answers) => {
             if(answers.role === 'Engineer') {
                 let eng = new Engineer(answers);
