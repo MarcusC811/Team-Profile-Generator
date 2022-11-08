cbc6d506bce7864d3c0af5ec48223c130e63ba08
OVOMARK <mcncstatefan@icloud.com>
2022-11-08 13:28:18 -0500

    headache

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
+let team = []
+
 let questions = [{
     type: 'input',
     message: 'What is your name?',
@@ -22,24 +24,49 @@ let questions = [{
     type: 'input',
     message: 'What is their email?',
     name: 'Email'
+},
+{
+    type: 'list',
+    message: 'What is your role?',
+    choices: ['Manager', 'Engineer', 'Intern', 'No more members'],
+    name: 'role'
 }]
 
+e
 function init () {
-    let team = []
+
+    let member = {};
 
         Inquirer
         .prompt(questions)
         .then((answers) => {
             if(answers.role === 'Engineer') {
-                let eng = new Engineer(answers);
-                eng.getGithub();
-            } else if(answers.role === 'Intern') {
-                let int = new Intern(answers)
-                int.getSchool();
-            } else if(answers.role === 'Manager') {
-                let man = new Manager(answers);
-                man.getOff();
-            }
+                const eQuest = getGithub();
+                
+                let eng = new Engineer(answers.name, answers.email, answers.id, eQuest.GitHub);
+                // eng.getGithub(answers.name, answers.email, answers.id);
+                team.push(eng)
+            } 
+            
         })
 }
+
+function getGithub() {
+    Inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is your GitHub username?',
+                name: 'GitHub'
+            }
+        ])           
+}
 init();
+
+// else if(answers.role === 'Intern') {
+            //     let int = new Intern(answers)
+            //     int.getSchool();
+            // } else if(answers.role === 'Manager') {
+            //     let man = new Manager(answers);
+            //     man.getOff();
+            // }
