0054cdf5742cb3439d2c89bbca3716481abf98e4
OVOMARK <mcncstatefan@icloud.com>
2022-11-08 18:00:11 -0500

    updated html gen

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
 let team = []
+// let teamDiv = document.getElementsByClassName("addtome")
 
 let questions = [{
     type: 'input',
@@ -86,10 +87,80 @@ function init () {
 
 function addTeam (member) {
     team.push(member);
+
+    Inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'Would you like to add an employee?',
+            choices: ['Yes', 'No'],
+            name: 'YNEmployee'
+        }
+    ])
+    .then((answers) => {
+        if(answers.YNEmployee === 'Yes') {
+            init();
+        } else if(answers.YNEmployee === 'No') {
+            genHTML(team);
+        }
+    })
 }
 
 function genHTML (team) {
-    let content = ``
+    let content = 
+    `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    </head>
+    <body>
+        <header>
+            <h1>My Team</h1>
+        </header>
+    
+        <main class="addtome">
+              <div class="card" style="width: 18rem;">
+                  <div class="card-header">
+                    <p>${team[0].Name}</p>
+                    <p>${team[0].role}</p>
+                  </div>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${team[0].ID}</li>
+                    <li class="list-group-item"><a href = "mailto: ${team[0].Email}">${team[0].Email}</a> </li>
+                    <li class="list-group-item">Office Number: ${team[0].offNum}</a></li>
+                  </ul>
+              </div>
+
+              <div class="card" style="width: 18rem;">
+                  <div class="card-header">
+                    <p>${team[1].Name}</p>
+                    <p>${team[1].role}</p>
+                  </div>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${team[1].ID}</li>
+                    <li class="list-group-item"><a href = "mailto: ${team[1].Email}">${team[1].Email}</a> </li>
+                    <li class="list-group-item">GitHub: <a href = "https://github.com/${team[1].GitHub}">${team[1].GitHub}</a></li>
+                  </ul>
+              </div>
+
+              <div class="card" style="width: 18rem;">
+                  <div class="card-header">
+                    <p>${team[2].Name}</p>
+                    <p>${team[2].role}</p>
+                  </div>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${team[2].ID}</li>
+                    <li class="list-group-item"><a href = "mailto: ${team[2].Email}">${team[2].Email}</a> </li>
+                    <li class="list-group-item">School: ${team[2].School}</li>
+                  </ul>
+              </div>
+        </main>
+    </body>
+    </html>`
     
     fs.writeFile('index.HTML', content, err => {
         if (err) {
